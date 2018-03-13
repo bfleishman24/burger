a870ea1ef14f2690121a9c25855e7a626f31ca85
Fence <Fence@ibp03mBF01.local>
2018-03-12 20:08:43 -0500

    almost finished Commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('Bacon And Cheese', false);
+INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Royal With Cheese', false);
+INSERT INTO burgers (name, devoured) VALUES ('Avavado Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Elvis Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Mac And Cheese Burger', false);
